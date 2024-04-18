f4303b0e411b7d65d75df99f8042e97ff66d3496
firas hamila <firas.hm.1996@gmail.com>
2024-04-18 17:27:51 +0200

    aa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -6,12 +6,22 @@ import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  // console.log("abc");
+  useEffect(() => {
+    // console.log("effect");
+    const x = localStorage.getItem("isLoggedIn");
+    if (x === "abc") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  // console.log("123");
   const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "abc");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
