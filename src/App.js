14fe11fa646de70f5d892f38c254dc8994f88fbf
firas hamila <firas.hm.1996@gmail.com>
2024-05-02 15:39:03 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -6,40 +6,15 @@ import MainHeader from "./components/MainHeader/MainHeader";
 import AuthStore from "./store/auth-store";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // console.log("abc");
-  useEffect(() => {
-    // console.log("effect");
-    const x = localStorage.getItem("isLoggedIn");
-    if (x === "abc") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-  // console.log("123");
-  const loginHandler = () => {
-    localStorage.setItem("isLoggedIn", "abc");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
-
+  const { isLoggedIn } = useContext(AuthStore);
   return (
-    <AuthStore.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-        loginHandler: loginHandler,
-        logoutHandler: logoutHandler,
-      }}
-    >
+    <>
       <MainHeader />
       <main>
         {!isLoggedIn && <Login />}
         {isLoggedIn && <Home />}
       </main>
-    </AuthStore.Provider>
+    </>
   );
 }
 
--- src/store/auth-store.js
+++ src/store/auth-store.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const AuthStore = createContext({
   //   email: "",
@@ -8,3 +8,36 @@ const AuthStore = createContext({
   logoutHandler: () => {},
 });
 export default AuthStore;
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // console.log("abc");
+  useEffect(() => {
+    // console.log("effect");
+    const x = localStorage.getItem("isLoggedIn");
+    if (x === "abc") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  // console.log("123");
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "abc");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  return (
+    <AuthStore.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        loginHandler: loginHandler,
+        logoutHandler: logoutHandler,
+      }}
+    >
+      {children}
+    </AuthStore.Provider>
+  );
+};
